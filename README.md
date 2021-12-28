32844ab5f7cb91121b41ec71ee84bfef325b665e
addshore <addshorewiki@gmail.com>
2021-12-28 14:35:09 +0000

    Initial commit with starter projects

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # wikicrowd
+
+Install flutter https://docs.flutter.dev/get-started/install (2.8.1)
+
+Created flutter app
+
+```sh
+flutter create ui
+```
+
+And run it in a browser
+
+```sh
+flutter run
+```
+
+
+```sh
+# Execute Docker services detaching the terminal
+docker-compose up -d
+```
---
+++ app/config/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'mariadb'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
