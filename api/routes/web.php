4a391c1d2d168a60478256902623d04e106c1a28
addshore <addshorewiki@gmail.com>
2021-12-30 16:29:13 +0000

    Make an edit

--- api/routes/web.php
+++ api/routes/web.php
@@ -22,5 +22,21 @@ Route::middleware('auth:sanctum')->get('/edit', function () {
 })->name('edit');
 
 Route::middleware('auth:sanctum')->post('/edit', function () {
-    return "posted!";
+    $user = Auth::user();
+    $mwAuth = new \Addwiki\Mediawiki\Api\Client\Auth\OAuthOwnerConsumer(
+        config('services.mediawiki.identifier'),
+        config('services.mediawiki.secret'),
+        $user->token,
+        $user->token_secret
+    );
+
+    $mw = \Addwiki\Mediawiki\Api\Client\MediaWiki::newFromEndpoint( 'https://addshore-alpha.wiki.opencura.com/w/api.php', $mwAuth );
+    $mwServices = new \Addwiki\Mediawiki\Api\MediawikiFactory( $mw->action() );
+
+    $result = $mwServices->newRevisionSaver()->save( new \Addwiki\Mediawiki\DataModel\Revision(
+        new \Addwiki\Mediawiki\DataModel\Content( 'Hello World (from laravel)' ),
+        new \Addwiki\Mediawiki\DataModel\PageIdentifier( new \Addwiki\Mediawiki\DataModel\Title( 'From Laravel Via OAuth' ) )
+    ) );
+
+    return "posted and got result ${result}!";
 });
