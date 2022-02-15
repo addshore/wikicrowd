c55778ee051a37801de8cff40adc0fcbdf3e41a1
addshore <addshorewiki@gmail.com>
2022-02-15 23:35:50 +0000

    Add access keys for yes no skip buttons

--- api/resources/views/components/yes-no-maybe-buttons.blade.php
+++ api/resources/views/components/yes-no-maybe-buttons.blade.php
@@ -2,17 +2,17 @@
     @csrf
     <input name="question" type="hidden" value="{{ $quId }}">
     <input name="answer" type="hidden" value="yes">
-    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Yes</button>
+    <button accesskey="y" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Yes</button>
 </form>
 <form id="no-form" action="{{ route('answers') }}" method="POST">
     @csrf
     <input name="question" type="hidden" value="{{ $quId }}">
     <input name="answer" type="hidden" value="no">
-    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">No</button>
+    <button accesskey="n" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">No</button>
 </form>
 <form class="pl-2" id="skip-form" action="{{ route('answers') }}" method="POST">
     @csrf
     <input name="question" type="hidden" value="{{ $quId }}">
     <input name="answer" type="hidden" value="skip">
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Skip</button>
+    <button accesskey="s" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Skip</button>
 </form>
