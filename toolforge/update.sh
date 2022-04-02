0b7cecef92d0ed266940b1f1b0163af18c86e8c5
addshore <addshorewiki@gmail.com>
2022-04-02 10:51:56 +0100

    toolforge: restructe update.sh

--- toolforge/update.sh
+++ toolforge/update.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+
+# Get latest code
 git -C ~/src pull
+
+# Install dependencies & prep files
 webservice php7.4 shell -- composer install --no-dev --working-dir=./src
 webservice node12 shell -- npm --prefix src install
 webservice node12 shell -- npm --prefix src run production
+
+# Copy files to correct location
+rsync -av --delete --exclude 'storage/framework/*' --exclude 'node_modules/*' -exclude '.git/*' ~/src/ ~/public_html
+
+# TODO migrate if needed
+
+# Restart the web server
 cp ~/src/toolforge/lighttpd.conf ~/.lighttpd.conf
 cp ~/src/toolforge/service.template ~/service.template
-cp ~/src/toolforge/deployment.yaml ~/deployment.yaml
-rsync -av --delete --exclude 'storage/framework/*' --exclude 'node_modules/*' -exclude '.git/*' ~/src/ ~/public_html
-# TODO migrate if needed
 webservice restart
-kubectl delete deployment laravel.queue
+
+# Re apply k8s Deployments
+kubectl delete -f ~/deployment.yaml
+cp ~/src/toolforge/deployment.yaml ~/deployment.yaml
 kubectl create --validate=true -f ~/deployment.yaml
