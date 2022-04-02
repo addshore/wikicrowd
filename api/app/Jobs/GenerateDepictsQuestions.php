0451c102b84c9bb4dfdd1df3438239fce17a547e
addshore <addshorewiki@gmail.com>
2022-04-02 08:55:37 +0100

    Use mediainfo id for image links

--- api/app/Jobs/GenerateDepictsQuestions.php
+++ api/app/Jobs/GenerateDepictsQuestions.php
@@ -314,7 +314,6 @@ class GenerateDepictsQuestions implements ShouldQueue
                     'old_depicts_name' => $lessSpecificItemLabel->getText(),
                     'depicts_id' => $this->depictItemId,
                     'depicts_name' => $this->depictName,
-                    'img_title' => $filePageIdentifier->getTitle()->getText(),
                     'img_url' => $thumbUrl,
                 ]
             ]);
@@ -333,7 +332,6 @@ class GenerateDepictsQuestions implements ShouldQueue
                 'mediainfo_id' => $mid->getSerialization(),
                 'depicts_id' => $this->depictItemId,
                 'depicts_name' => $this->depictName,
-                'img_title' => $filePageIdentifier->getTitle()->getText(),
                 'img_url' => $thumbUrl,
             ]
         ]);
