23271c6a6310fefe633e39fe4d3fa6364ddb7535
addshore <addshorewiki@gmail.com>
2022-04-03 21:21:35 +0100

    Migration to fix old questions for gliders
    Follow up to 7949f43170ad8ef4b5a4589abde5adb511a36e22

---
+++ database/migrations/2022_04_03_000000_update_depicts_questions_for_glider_item_id_fix.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use App\Models\QuestionGroup;
+use App\Models\Question;
+
+/**
+ * One off migration to enable alignment of previously generated questions for the glider ID fix https://github.com/addshore/wikicrowd/commit/7949f43170ad8ef4b5a4589abde5adb511a36e22
+ */
+class UpdateDepictsQuestionsForGliderItemIdFix extends Migration
+{
+
+    private $oldId = 'Q8492796';
+    private $newId = 'Q2165278';
+
+    public function up()
+    {
+        $depictsGroup = QuestionGroup::where('name', '=', 'depicts/' . $this->oldId)->first();
+        $depictsGroup->name = str_replace($this->oldId,$this->newId, $depictsGroup->name);
+        $depictsGroup->save();
+        if($depictsGroup !== null) {
+            $questions = Question::where('question_group_id', '=', $depictsGroup->id)->get();
+            foreach( $questions as $question ) {
+                $question->unique_id = str_replace($this->oldId,$this->newId, $question->unique_id);
+                $question->properties = $this->arrayWithCallbackApplieOnKey($question->properties, 'depicts_id', function($did) {
+                    return $this->newId;
+                });
+                $question->save();
+            }
+        }
+
+        // At the time of writing this there was only depicts questions, so only rename the refine group!
+        $depictsRefineGroup = QuestionGroup::where('name', '=', 'depicts-refine/Q8492796')->first();
+        $depictsGroup->name = str_replace($this->oldId,$this->newId, $depictsGroup->name);
+        $depictsRefineGroup->save();
+    }
+
+    public function down()
+    {
+        // No way back for this one...
+    }
+
+    private function arrayWithCallbackApplieOnKey( array $array, string $key, $callback ) : array {
+        $array[$key] = $callback($array[$key]);
+        return $array;
+    }
+
+}
