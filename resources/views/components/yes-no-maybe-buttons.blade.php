32f5716c11051839e2db4785b20a91b5da8be5a4
Nux <eccenux+git@gmail.com>
2023-04-24 19:08:44 +0200

    Change buttons to YES-SKIP-NO #81

--- resources/views/components/yes-no-maybe-buttons.blade.php
+++ resources/views/components/yes-no-maybe-buttons.blade.php
@@ -7,15 +7,6 @@
     @endif
     <button accesskey="y" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" title="Press 1 to execute">Yes [1]</button>
 </form>
-<form id="no-form" action="{{ route('answers') }}" method="POST">
-    @csrf
-    <input name="question" type="hidden" value="{{ $quId }}">
-    <input name="answer" type="hidden" value="no">
-    @if( isset($nextId) && $nextId != null )
-        <input name="next" type="hidden" value="{{ $nextId }}">
-    @endif
-    <button accesskey="n" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" title="Press 2 to execute">No [2]</button>
-</form>
 <form class="pl-2" id="skip-form" action="{{ route('answers') }}" method="POST">
     @csrf
     <input name="question" type="hidden" value="{{ $quId }}">
@@ -23,7 +14,16 @@
     @if( isset($nextId) && $nextId != null )
         <input name="next" type="hidden" value="{{ $nextId }}">
     @endif
-    <button accesskey="s" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" title="Press 3 to execute">Skip [3]</button>
+    <button accesskey="s" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" title="Press 2 to execute">Skip [2]</button>
+</form>
+<form id="no-form" action="{{ route('answers') }}" method="POST">
+    @csrf
+    <input name="question" type="hidden" value="{{ $quId }}">
+    <input name="answer" type="hidden" value="no">
+    @if( isset($nextId) && $nextId != null )
+        <input name="next" type="hidden" value="{{ $nextId }}">
+    @endif
+    <button accesskey="n" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" title="Press 3 to execute">No [3]</button>
 </form>
 
 <script>
@@ -68,8 +68,8 @@ class KeyboardActions {
 		// Note, keep this mapping in sync with labels.
 		switch (character) {
 			case '1': action = 'yes'; break;
-			case '2': action = 'no'; break;
-			case '3': action = 'skip'; break;
+			case '2': action = 'skip'; break;
+			case '3': action = 'no'; break;
 		}
 		return action;
 	}
