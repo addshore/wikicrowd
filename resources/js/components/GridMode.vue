e69c45fa63ccd5fa6890f8e83f067b429da031c8
addshore <addshorewiki@gmail.com>
2025-05-07 20:00:57 +0100

    Reduce column count on mobile for grid
    Fixes #97

--- resources/js/components/GridMode.vue
+++ resources/js/components/GridMode.vue
@@ -29,7 +29,7 @@
         >Back to Focus/One</button>
       </div>
     </div>
-    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
       <div v-for="image in images" :key="image.id"
         @click="!answered.has(image.id) && toggleSelect(image.id)"
         :class="[
