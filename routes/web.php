d6901c327cf2c4e90c041b497bb777e7434f60c1
addshore <addshorewiki@gmail.com>
2025-05-25 22:29:52 +0100

    Remove the old web answers route

--- routes/web.php
+++ routes/web.php
@@ -43,55 +43,6 @@ Route::middleware('auth:sanctum')->get('/questions/depicts/custom', function () 
 Route::middleware('auth:sanctum')->get('/questions/{groupName}/{desiredId?}', [QuestionController::class, 'showGroupDesiredOrUnanswered'])
     ->where('groupName', '([^\/]*\/[^\/]*)');
 
-// TODO also move this to the API
-Route::middleware('auth:sanctum')->name('answers')->post('/answers', function (Request $request) {
-
-    $v = Validator::make($request->all(), [
-        'question' => 'required|exists:App\Models\Question,id',
-        // TODO make this generic (from group?)
-        'answer' => 'required|in:yes,no,skip',
-        'next' => 'exists:App\Models\Question,id'
-    ]);
-    if ($v->fails()) {
-        die("unexpected submission");
-    }
-
-    $question = $request->input('question');
-    $answer = $request->input('answer');
-    $user = Auth::user();
-
-    // Get the question
-    $question = Question::find($question);
-
-    // Store the answer
-    $storedAnswer = Answer::create([
-        'question_id' => $question->id,
-        'user_id' => $user->id,
-        'answer' => $answer,
-    ]);
-
-    // Dispatch the edit job..
-    if($answer === 'yes') {
-        // TODO make a generic answer -> edit job?
-        if($question->group->parentGroup->name === 'depicts') {
-            dispatch(new AddDepicts($storedAnswer->id));
-        }
-        if($question->group->parentGroup->name === 'depicts-refine') {
-            dispatch(new SwapDepicts($storedAnswer->id));
-        }
-        if($question->group->parentGroup->name === 'aliases') {
-            dispatch(new AddAlias($storedAnswer->id));
-        }
-    }
-
-    // Show the next one!
-    $next = $request->input('next');
-    if($next){
-        return redirect('/questions/' . $question->group->name . '/' . $next);
-    }
-    return redirect('/questions/' . $question->group->name);
-});
-
 Route::middleware('auth:sanctum')->get('/manual-question-grid', function () {
     return view('manual-question-grid');
 });
