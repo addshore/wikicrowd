37668bf0f41312246f03b362116a181bfb63c1da
addshore <addshorewiki@gmail.com>
2025-06-29 11:02:01 +0100

    stacks into daily logs....

--- config/logging.php
+++ config/logging.php
@@ -49,7 +49,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily'],
             'ignore_exceptions' => false,
         ],
 
