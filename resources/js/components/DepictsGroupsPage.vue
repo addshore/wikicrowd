1f4f5da7802a6d202150578bce1ffc3dc9c0e819
addshore <addshore@users.noreply.github.com>
2025-09-11 20:58:47 +0100

    link to edit groups in footer

--- resources/js/components/DepictsGroupsPage.vue
+++ resources/js/components/DepictsGroupsPage.vue
@@ -28,6 +28,7 @@
         <p>
           Commons:&nbsp;
           <a target="_blank" href="https://commons.wikimedia.org/w/index.php?hidebots=1&translations=filter&hideWikibase=1&tagfilter=OAuth+CID%3A+2642&limit=500&days=7&title=Special:RecentChanges&urlversion=2" class="text-blue-700 dark:text-blue-400 underline">All edits</a>
+          <a target="_blank" href="https://editgroups-commons.toolforge.org/?tool=wikicrowd" class="text-blue-700 dark:text-blue-400 underline">Edit groups</a>
           <span v-if="isAuthed">/
             <a target="_blank" href="https://commons.wikimedia.org/w/index.php?hidebyothers=1&hidebots=1&translations=filter&hideWikibase=1&tagfilter=OAuth+CID%3A+2642&limit=500&days=7&title=Special%3ARecentChanges&urlversion=2" class="text-blue-700 dark:text-blue-400 underline">Your edits</a>
           </span>
